6f3d993169df777bbfc814069a1f0883870a9eca
Clayton A. Alves <clayton.aa@gmail.com>
2012-07-04 20:44:58 -0400

    Adicionado script de instalação de pacotes

---
+++ install_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get install wine radiotray curl nmap \
+    python-tk python-mysqldb python-dev python-pip \
+    ipython mercurial git mysql-client p7zip \
+    monodevelop monodoc-browser shutter skype tmux
+    #mysql-server p7zip
+
+sudo pip install mailer
+sudo pip install django
+
+sudo apt-get install vim
+sudo apt-get install vim-gnome
+#sudo apt-get --purge remove overlay-scrollbar liboverlay-scrollbar-*
+sudo apt-get --purge remove mint-flash*
+sudo apt-get install adobe-flashplugin
